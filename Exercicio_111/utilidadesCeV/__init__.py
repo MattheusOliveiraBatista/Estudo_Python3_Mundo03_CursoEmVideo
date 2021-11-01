2de4f140d2f4638b569c83ed0e268fffa1be55bb
MattheusOliveiraBatista <70457897+MattheusOliveiraBatista@users.noreply.github.com>
2021-10-31 21:37:51 -0300

    Delete Exercicio_111/utilidadesCeV directory

--- Exercicio_111/utilidadesCeV/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
-Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando.
-"""
-from Exercicio_111.utilidadesCeV import moeda
-
-print('\033[1;31m-\033[m' * 30)
-p = float(input('Digite seu preço: R$ '))
-moeda.resumo(p)
